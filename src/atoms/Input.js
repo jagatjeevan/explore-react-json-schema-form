99cd241ec8d424ef55346fbb17b36bb6ea921b74
Jagat Jeevan Sahoo <kanhajeevan@gmail.com>
2021-09-29 08:06:12 +0530

    With async of the function

--- src/atoms/Input.js
+++ src/atoms/Input.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from "react";
 
-const Input = props => {
-    const { value } = props;
+const Input = (props) => {
+  const { value } = props;
 
-    const onChange = (event) => {
-        props.onChange(event.target.value)
-    }
+  const onChange = (event) => {
+    props.onChange(event.target.value);
+  };
 
-    const onBlur = () => {
-        const trimmedValue = value.trim()
-        props.onChange(trimmedValue)
-        props.onBlur()
-    }
+  const onBlur = async () => {
+    const trimmedValue = value.trim();
+    await props.onChange(trimmedValue);
+    props.onBlur();
+  };
 
-    return (
-        <div>
-            <input type='text' value={value} onChange={onChange} onBlur={onBlur} />
-        </div>
-    );
+  return (
+    <div>
+      <input type="text" value={value} onChange={onChange} onBlur={onBlur} />
+    </div>
+  );
 };
 
 export default Input;
